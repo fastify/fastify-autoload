af10a360211c6aef3e39bb962f98661b13f50bae
Arniu <arniu2006@gmail.com>
2018-12-19 13:24:31 +0800

    Format code with prettier

--- index.js
+++ index.js
@@ -12,60 +12,68 @@ module.exports = function (fastify, opts, next) {
       return
     }
 
-    steed.map(list, (file, cb) => {
-      const toLoad = path.join(opts.dir, file)
-      fs.stat(toLoad, (err, stat) => {
-        if (err) {
-          cb(err)
-          return
-        }
-
-        cb(null, { file: toLoad, stat })
-      })
-    }, (err, stats) => {
-      if (err) {
-        next(err)
-        return
-      }
-      for (let i = 0; i < stats.length; i++) {
-        const { stat, file } = stats[i]
-
-        if (stat.isFile() && (!file.match(/.js$/) || (opts.ignorePattern && file.match(opts.ignorePattern)))) {
-          continue
-        }
-
-        // TODO handle empty directories
-        // TODO handle directories with a package.json
-        if (stat.isFile() || stat.isDirectory()) {
-          try {
-            const plugin = require(file)
-            const pluginOptions = {}
-            Object.assign(pluginOptions, defaultPluginOptions)
-            if (plugin.autoPrefix) {
-              const prefix = pluginOptions.prefix || ''
-              pluginOptions.prefix = prefix + plugin.autoPrefix
-            }
-            if (plugin.prefixOverride !== void 0) {
-              pluginOptions.prefix = plugin.prefixOverride
-            }
-            if (plugin.autoload !== false) {
-              fastify.register(plugin, pluginOptions)
-            }
-          } catch (err) {
-            // Hack SyntaxError message so that we provide
-            // the line number to the user, otherwise they
-            // will be left in the cold.
-            if (err instanceof SyntaxError) {
-              err.message += ' at ' + err.stack.split('\n')[0]
-            }
-            next(err)
+    steed.map(
+      list,
+      (file, cb) => {
+        const toLoad = path.join(opts.dir, file)
+        fs.stat(toLoad, (err, stat) => {
+          if (err) {
+            cb(err)
             return
           }
-        }
-      }
 
-      next()
-    })
+          cb(null, { file: toLoad, stat })
+        })
+      },
+      (err, stats) => {
+        if (err) {
+          next(err)
+          return
+        }
+        for (let i = 0; i < stats.length; i++) {
+          const { stat, file } = stats[i]
+
+          if (
+            stat.isFile() &&
+            (!file.match(/.js$/) ||
+              (opts.ignorePattern && file.match(opts.ignorePattern)))
+          ) {
+            continue
+          }
+
+          // TODO handle empty directories
+          // TODO handle directories with a package.json
+          if (stat.isFile() || stat.isDirectory()) {
+            try {
+              const plugin = require(file)
+              const pluginOptions = {}
+              Object.assign(pluginOptions, defaultPluginOptions)
+              if (plugin.autoPrefix) {
+                const prefix = pluginOptions.prefix || ''
+                pluginOptions.prefix = prefix + plugin.autoPrefix
+              }
+              if (plugin.prefixOverride !== void 0) {
+                pluginOptions.prefix = plugin.prefixOverride
+              }
+              if (plugin.autoload !== false) {
+                fastify.register(plugin, pluginOptions)
+              }
+            } catch (err) {
+              // Hack SyntaxError message so that we provide
+              // the line number to the user, otherwise they
+              // will be left in the cold.
+              if (err instanceof SyntaxError) {
+                err.message += ' at ' + err.stack.split('\n')[0]
+              }
+              next(err)
+              return
+            }
+          }
+        }
+
+        next()
+      }
+    )
   })
 }
 
--- test/basic.js
+++ test/basic.js
@@ -12,86 +12,116 @@ app.register(require('./basic/app'))
 app.ready(function (err) {
   t.error(err)
 
-  app.inject({
-    url: '/something'
-  }, function (err, res) {
-    t.error(err)
+  app.inject(
+    {
+      url: '/something'
+    },
+    function (err, res) {
+      t.error(err)
 
-    t.equal(res.statusCode, 200)
-    t.deepEqual(JSON.parse(res.payload), { something: 'else' })
-  })
+      t.equal(res.statusCode, 200)
+      t.deepEqual(JSON.parse(res.payload), { something: 'else' })
+    }
+  )
 
-  app.inject({
-    url: '/foo/bar'
-  }, function (err, res) {
-    t.error(err)
+  app.inject(
+    {
+      url: '/foo/bar'
+    },
+    function (err, res) {
+      t.error(err)
 
-    t.equal(res.statusCode, 200)
-    t.deepEqual(JSON.parse(res.payload), { foo: 'bar' })
-  })
+      t.equal(res.statusCode, 200)
+      t.deepEqual(JSON.parse(res.payload), { foo: 'bar' })
+    }
+  )
 
-  app.inject({
-    url: '/prefixed'
-  }, function (err, res) {
-    t.error(err)
+  app.inject(
+    {
+      url: '/prefixed'
+    },
+    function (err, res) {
+      t.error(err)
 
-    t.equal(res.statusCode, 200)
-    t.deepEqual(JSON.parse(res.payload), { something: 'else' })
-  })
+      t.equal(res.statusCode, 200)
+      t.deepEqual(JSON.parse(res.payload), { something: 'else' })
+    }
+  )
 
-  app.inject({
-    url: '/skip'
-  }, function (err, res) {
-    t.error(err)
-    t.equal(res.statusCode, 404)
-  })
+  app.inject(
+    {
+      url: '/skip'
+    },
+    function (err, res) {
+      t.error(err)
+      t.equal(res.statusCode, 404)
+    }
+  )
 
-  app.inject({
-    url: '/options'
-  }, function (err, res) {
-    t.error(err)
-    t.equal(res.statusCode, 200)
-    t.deepEqual(JSON.parse(res.payload), {
-      foo: 'bar'
-    })
-  })
+  app.inject(
+    {
+      url: '/options'
+    },
+    function (err, res) {
+      t.error(err)
+      t.equal(res.statusCode, 200)
+      t.deepEqual(JSON.parse(res.payload), {
+        foo: 'bar'
+      })
+    }
+  )
 
-  app.inject({
-    url: '/defaultPrefix'
-  }, function (err, res) {
-    t.error(err)
-    t.equal(res.statusCode, 200)
-    t.deepEqual(JSON.parse(res.payload), { index: true })
-  })
+  app.inject(
+    {
+      url: '/defaultPrefix'
+    },
+    function (err, res) {
+      t.error(err)
+      t.equal(res.statusCode, 200)
+      t.deepEqual(JSON.parse(res.payload), { index: true })
+    }
+  )
 
-  app.inject({
-    url: '/defaultPrefix/prefixed'
-  }, function (err, res) {
-    t.error(err)
-    t.equal(res.statusCode, 200)
-    t.deepEqual(JSON.parse(res.payload), { prefixed: true })
-  })
+  app.inject(
+    {
+      url: '/defaultPrefix/prefixed'
+    },
+    function (err, res) {
+      t.error(err)
+      t.equal(res.statusCode, 200)
+      t.deepEqual(JSON.parse(res.payload), { prefixed: true })
+    }
+  )
 
-  app.inject({
-    url: '/defaultPrefix/overriddenPrefix'
-  }, function (err, res) {
-    t.error(err)
-    t.equal(res.statusCode, 404)
-  })
+  app.inject(
+    {
+      url: '/defaultPrefix/overriddenPrefix'
+    },
+    function (err, res) {
+      t.error(err)
+      t.equal(res.statusCode, 404)
+    }
+  )
 
-  app.inject({
-    url: '/overriddenPrefix'
-  }, function (err, res) {
-    t.error(err)
-    t.equal(res.statusCode, 200)
-    t.deepEqual(JSON.parse(res.payload), { overide: 'prefix' })
-  })
+  app.inject(
+    {
+      url: '/overriddenPrefix'
+    },
+    function (err, res) {
+      t.error(err)
+      t.equal(res.statusCode, 200)
+      t.deepEqual(JSON.parse(res.payload), { overide: 'prefix' })
+    }
+  )
 
-  app.inject({
-    url: '/noPrefix'
-  }, function (err, res) {
-    t.error(err)
-    t.equal(res.statusCode, 200)
-    t.deepEqual(JSON.parse(res.payload), { no: 'prefix' })
-  })
+  app.inject(
+    {
+      url: '/noPrefix'
+    },
+    function (err, res) {
+      t.error(err)
+      t.equal(res.statusCode, 200)
+      t.deepEqual(JSON.parse(res.payload), { no: 'prefix' })
+    }
+  )
 })
