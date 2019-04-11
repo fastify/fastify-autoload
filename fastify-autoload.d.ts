687869649268eab6619496c2e282b2779a3ae701
Gang Lv <gang.lv@shopee.com>
2019-04-11 11:44:13 +0800

    feat(*): add types test case

--- fastify-autoload.d.ts
+++ fastify-autoload.d.ts
@@ -2,10 +2,10 @@ import { Plugin, RegisterOptions } from 'fastify'
 import { Server, IncomingMessage, ServerResponse} from 'http'
 declare namespace fastifyAutoload {
     interface PluginOptions {
-        dir?: string
-        ignorePattern?: string
+        dir: string
+        ignorePattern?: RegExp | string
         options?: RegisterOptions<Server, IncomingMessage, ServerResponse>
-    }
+    } 
 }
 
 declare const fastifyAutoload: Plugin<Server, IncomingMessage, ServerResponse, fastifyAutoload.PluginOptions>;
