cb1da9179675731754b5573b3d6ce37adb326588
kidyfirst <kidyfirst@126.com>
2019-04-12 15:18:40 +0800

    support types and add types case (#28)
    * feat(*): add types and support the es6 module
    
    * chore(*): delete the idea
    
    * feat(*): remove yarn.lock
    
    * feat(*): standard the index
    
    * feat(*): add types test case
    
    * chore(*): update the denpendence module

---
+++ types.test.ts
@@ -0,0 +1,20 @@
+import * as autoload from '../fastify-autoload';
+import * as fastify from 'fastify';
+
+const app = fastify()
+let opt1: autoload.PluginOptions = {
+    dir: 'test'
+}
+const opt2: autoload.PluginOptions = {
+    dir: 'test',
+    ignorePattern: /skip/
+}
+const opt3: autoload.PluginOptions = {
+    dir: 'test',
+    options: {
+        prefix: 'test'
+    }
+}
+app.register(autoload, opt1)
+app.register(autoload, opt2)
+app.register(autoload, opt3)
