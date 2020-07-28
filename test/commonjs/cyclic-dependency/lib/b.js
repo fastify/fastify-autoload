f1f84371703bad7a1d0423e7861ca1a29464eb94
Rafael Gonzaga <rafael.nunu@hotmail.com>
2020-07-28 04:55:26 -0300

    test: add cyclic-dependency and module-error tests (#96)

---
+++ test/commonjs/cyclic-dependency/lib/b.js
@@ -0,0 +1,13 @@
+'use strict'
+
+const fp = require('fastify-plugin')
+
+module.exports = fp(function (fastify, opts, next) {
+  fastify.get('/b', function (_request, reply) {
+    reply.send(opts)
+  })
+  next()
+}, {
+  dependencies: ['plugin-a'],
+  name: 'plugin-b'
+})
