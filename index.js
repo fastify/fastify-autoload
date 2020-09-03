157a8dcb62944ccb43b7a72b62d79d31ed0c1690
Michiel De Mey <de.mey.michiel@gmail.com>
2020-09-03 20:18:43 +0200

    Added support for ts-node-dev (#100)

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const { readdir } = require('fs').promises
 const pkgUp = require('pkg-up')
 const semver = require('semver')
 
-const isTsNode = Symbol.for('ts-node.register.instance') in process
+const isTsNode = (Symbol.for('ts-node.register.instance') in process) || !!process.env.TS_NODE_DEV
 const isJestEnviroment = process.env.JEST_WORKER_ID !== undefined
 const hasTsJest = 'npm_package_devDependencies_ts_jest' in process.env
 const typescriptSupport = isTsNode || (isJestEnviroment && hasTsJest)
