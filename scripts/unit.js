661dde0c3b6164d8abdd22aeae959434b012f26a
Simone Busoli <simone.busoli@gmail.com>
2021-03-08 13:05:19 +0100

    chore: add cross-os script to run unit tests

---
+++ scripts/unit.js
@@ -0,0 +1,11 @@
+'use strict'
+
+const { exec } = require('child_process')
+const semver = require('semver')
+
+const child = exec(semver.satisfies('>=14 || >= 12.17.0 < 13.0.0')
+  ? 'npm run unit:with-modules'
+  : 'npm run unit:without-modules')
+
+child.stdout.pipe(process.stdout)
+child.stderr.pipe(process.stderr)
