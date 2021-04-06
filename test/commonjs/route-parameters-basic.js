5973e3ca6b5701c6860389cbf9896241f95b4fc6
Polymath Interactive <bill@polymain.com>
2021-04-06 11:35:44 -0300

    adding cjs test for route parameters

---
+++ test/commonjs/route-parameters-basic.js
@@ -0,0 +1,41 @@
+'use strict'
+
+const t = require('tap')
+const Fastify = require('fastify')
+
+t.plan(10)
+
+const app = Fastify()
+
+app.register(require('./route-parameters/basic'))
+
+app.ready(function (err) {
+  t.error(err)
+
+  app.inject({
+    url: '/users'
+  }, function (err, res) {
+    t.error(err)
+
+    t.equal(res.statusCode, 200)
+    t.deepEqual(JSON.parse(res.payload), { users: [{ id: 7, username: 'example' }] })
+  })
+
+  app.inject({
+    url: '/users/7'
+  }, function (err, res) {
+    t.error(err)
+
+    t.equal(res.statusCode, 200)
+    t.deepEqual(JSON.parse(res.payload), { user: { id: 7, username: 'example' } })
+  })
+
+  app.inject({
+    url: '/users/_id'
+  }, function (err, res) {
+    t.error(err)
+
+    t.equal(res.statusCode, 200)
+    t.deepEqual(JSON.parse(res.payload), { user: { id: '_id', username: 'example' } })
+  })
+})
---
+++ test/commonjs/route-parameters-disabled.js
@@ -0,0 +1,40 @@
+'use strict'
+
+const t = require('tap')
+const Fastify = require('fastify')
+
+t.plan(9)
+
+const app = Fastify()
+
+app.register(require('./route-parameters/disabled'))
+
+app.ready(function (err) {
+  t.error(err)
+
+  app.inject({
+    url: '/users'
+  }, function (err, res) {
+    t.error(err)
+
+    t.equal(res.statusCode, 200)
+    t.deepEqual(JSON.parse(res.payload), { users: [{ id: 7, username: 'example' }] })
+  })
+
+  app.inject({
+    url: '/users/7'
+  }, function (err, res) {
+    t.error(err)
+
+    t.equal(res.statusCode, 404)
+  })
+
+  app.inject({
+    url: '/users/_id'
+  }, function (err, res) {
+    t.error(err)
+
+    t.equal(res.statusCode, 200)
+    t.deepEqual(JSON.parse(res.payload), { user: { id: 'null', username: 'example' } })
+  })
+})
