41429507b7f5736d7d963dc3d005ed9b03c7ef07
Matteo Collina <hello@matteocollina.com>
2021-05-26 09:23:34 +0200

    fix tests

--- test/typescript-esm/forceESM.ts
+++ test/typescript-esm/forceESM.ts
@@ -1,6 +1,6 @@
 import fastify from 'fastify'
-import { dirname, resolve } from 'node:path'
-import { fileURLToPath } from 'node:url'
+import { dirname, resolve } from 'path'
+import { fileURLToPath } from 'url'
 import t from 'tap'
 import fastifyAutoLoad from '../../'
 
