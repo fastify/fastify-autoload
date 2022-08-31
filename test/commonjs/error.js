c51f5c16e8c4ad51085e1eae462a6bd4170b163a
Kyle Rush <kylerrush@gmail.com>
2022-08-31 13:34:41 -0400

    Add support for .ts files when app is not run with ts-node.

--- test/commonjs/error.js
+++ test/commonjs/error.js
@@ -3,6 +3,7 @@
 const t = require('tap')
 const fastify = require('fastify')
 const semver = require('semver')
+const { join } = require('path')
 
 const moduleSupport = semver.satisfies(process.version, '>= 14 || >= 12.17.0 < 13.0.0')
 
@@ -28,11 +29,21 @@ t.test('independent of module support', function (t) {
 
   const app3 = fastify()
 
-  app3.register(require('./ts-error/app'))
+  const mockedAutoLoad = t.mock('../../', {
+    'ts-node': {
+      register: function () {
+        throw new Error('ts-node doesn\'t exist because it is mocked')
+      }
+    }
+  })
+
+  app3.register(mockedAutoLoad, {
+    dir: join(__dirname, 'ts-node/routes')
+  })
 
   app3.ready(function (err) {
     t.type(err, Error)
-    t.match(err.message, /cannot import plugin.*typescript/i)
+    t.match(err.message, /cannot import plugin.*typescript/i, 't.match error.message for app 3')
   })
 
   const app4 = fastify()
---
+++ test/commonjs/ts-node/routes/foo/index.ts
@@ -0,0 +1,5 @@
+module.exports.default = async (fastify: any) => {
+  fastify.get("/", function () {
+    return { foo: "bar" };
+  })
+};
