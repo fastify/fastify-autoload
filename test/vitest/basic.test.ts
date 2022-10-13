860edaadcc39f45adfe247a664951d0708c3e491
Uzlopak <aras.abbasi@googlemail.com>
2022-10-13 07:04:49 +0200

    forceESM for vitest (#274)
    * forceESM for vitest
    
    * Apply suggestions from code review

---
+++ test/vitest/basic.test.ts
@@ -0,0 +1,31 @@
+'use strict'
+
+import { describe, test, expect } from 'vitest'
+import Fastify from 'fastify'
+import AutoLoad from '../../index'
+import { join } from 'path'
+
+describe.concurrent("Vitest test suite", function () {
+  const app = Fastify()
+    app.register(AutoLoad, {
+      dir: join(__dirname, '../commonjs/ts-node/routes')
+    })
+
+  test("Test the root route", async function () {
+    const response = await app.inject({
+      method: 'GET',
+      url: '/'
+    })
+    expect(response.statusCode).toEqual(200)
+    expect(JSON.parse(response.payload)).toEqual({ hello: 'world' })
+  })
+
+  test("Test /foo route", async function () {
+    const response = await app.inject({
+      method: 'GET',
+      url: '/foo'
+    })
+    expect(response.statusCode).toBe(200)
+    expect(JSON.parse(response.payload)).toEqual({ foo: 'bar' })
+  })
+})
