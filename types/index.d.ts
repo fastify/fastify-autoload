44f0a98aaf5d56ac41131d6ce875595c3a767f3e
Uzlopak <aras.abbasi@googlemail.com>
2022-11-22 09:19:01 +0100

    move typings to types folder & make typings nodenext compatible (#283)
    * move typings to types folder & nodenext compatible
    
    * move RewritePefix into namespace

---
+++ types/index.d.ts
@@ -0,0 +1,33 @@
+import { FastifyPluginCallback, FastifyPluginOptions } from 'fastify'
+
+type FastifyAutoloadPlugin = FastifyPluginCallback<NonNullable<fastifyAutoload.AutoloadPluginOptions>>
+
+declare namespace fastifyAutoload {
+  type RewritePrefix = (folderParent: string, folderName: string) => string | boolean
+
+  export interface AutoloadPluginOptions {
+    dir: string
+    dirNameRoutePrefix?: boolean | RewritePrefix
+    ignorePattern?: RegExp
+    scriptPattern?: RegExp
+    indexPattern?: RegExp
+    options?: FastifyPluginOptions
+    maxDepth?: number
+    forceESM?: boolean
+    encapsulate?: boolean
+    autoHooks?: boolean
+    autoHooksPattern?: RegExp
+    cascadeHooks?: boolean
+    overwriteHooks?: boolean
+    routeParams?: boolean
+  }
+
+  export const fastifyAutoload: FastifyAutoloadPlugin
+  export { fastifyAutoload as default };
+}
+
+declare function fastifyAutoload(
+  ...params: Parameters<FastifyAutoloadPlugin>
+): ReturnType<FastifyAutoloadPlugin>;
+
+export = fastifyAutoload;
