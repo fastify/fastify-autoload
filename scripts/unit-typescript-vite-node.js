836ae1296e43a16e3d67d138327e8b98f4da6647
xeho91 <xeho91@pm.me>
2024-02-07 17:51:39 +0800

    Add tests for `vite-node`

---
+++ scripts/unit-typescript-vite-node.js
@@ -0,0 +1,17 @@
+'use strict'
+
+const { exec } = require('node:child_process')
+
+const args = [
+  'npx',
+  'vite-node',
+  'test/typescript/basic.ts'
+]
+
+const child = exec(args.join(' '), {
+  shell: true
+})
+
+child.stdout.pipe(process.stdout)
+child.stderr.pipe(process.stderr)
+child.once('close', process.exit)
