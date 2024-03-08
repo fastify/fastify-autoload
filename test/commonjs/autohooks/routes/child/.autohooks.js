7f6da9c82f8370c6912406aa699327eded1ef0fc
jean <jean.antoine.michelet@gmail.com>
2024-03-08 11:06:36 +0100

    fix: reverse changes in routes/child/.autohooks.js

--- test/commonjs/autohooks/routes/child/.autohooks.js
+++ test/commonjs/autohooks/routes/child/.autohooks.js
@@ -1,10 +1,7 @@
 module.exports = async function (app, opts, next) {
-  app.setNotFoundHandler(() => {
-    throw new Error("foo");
-  });
-
   app.addHook('onRequest', async (req, reply) => {
     req.hooked = req.hooked || []
     req.hooked.push('child')
+    next()
   })
 }
