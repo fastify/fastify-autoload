6a2a6be81be31e68af5e36a60542eaa91ac65f67
jean <jean.antoine.michelet@gmail.com>
2024-03-15 10:19:47 +0100

    Should be able to filter paths using string

--- test/issues/369/test.js
+++ test/issues/369/test.js
@@ -59,6 +59,38 @@ test('Should not accumulate plugin if doesn\'t comply to matchFilter', async (t)
   t.equal(res2.statusCode, 404)
 })
 
+test('Should be able to filter paths using string', async (t) => {
+  const app = Fastify()
+
+  app.register(autoload, {
+    dir: path.join(__dirname, 'routes-c'),
+    matchFilter: 'routes.js'
+  })
+
+  await app.ready()
+
+  const res = await app.inject({
+    url: '/'
+  })
+
+  t.equal(res.statusCode, 200)
+
+  const app2 = Fastify()
+
+  app2.register(autoload, {
+    dir: path.join(__dirname, 'routes-c'),
+    matchFilter: 'invalid-path'
+  })
+
+  await app2.ready()
+
+  const res2 = await app2.inject({
+    url: '/'
+  })
+
+  t.equal(res2.statusCode, 404)
+})
+
 test('Should not accumulate plugin if ignoreFilter is matched', async (t) => {
   const app = Fastify()
 
