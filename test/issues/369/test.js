c2a75895d1873c7445b4fed2d54980a0a5fd7ad7
jean <jean.antoine.michelet@gmail.com>
2024-03-16 08:35:51 +0100

    test: should not fail if argv and execArgv are falsy

--- test/issues/369/test.js
+++ test/issues/369/test.js
@@ -2,7 +2,25 @@
 
 const { test } = require('tap')
 const Fastify = require('fastify')
-const autoload = require('../../..')
+
+// should not fail if argv and execArgv are falsy
+function requireAutoload() {
+  const originalArgv = process.argv;
+  const originalExecArgv = process.execArgv;
+  
+  process.argv = false;
+  process.execArgv = false;
+
+  const autoload = require('../../..')
+
+  process.argv = originalArgv;
+  process.execArgv = originalExecArgv;
+
+  return autoload
+}
+
+const autoload = requireAutoload()
+
 const path = require('path')
 
 test('Should throw an error when trying to load invalid hooks', async (t) => {
