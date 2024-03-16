ce5ac35c37f0866b223791c2506d6ae40cddaea8
jean <jean.antoine.michelet@gmail.com>
2024-03-16 09:19:31 +0100

    fix: remove obsolet commented code

--- test/issues/369/routes/.autohooks.js
+++ test/issues/369/routes/.autohooks.js
@@ -6,12 +6,3 @@ module.exports = async function (app, opts) {
       req.hooked.push('root')
     })
 }
-
-// module.exports = new Promise((res) => {
-//     res(async function (app, opts) {
-//         app.decorate('foo', true)
-//         app.get('/a', async function (req, reply) {
-//           reply.send()
-//         })
-//     })
-// })
