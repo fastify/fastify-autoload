e73b30d04ef57c52a2bdf072e7c5564f6eb56f95
jean <jean.antoine.michelet@gmail.com>
2024-04-19 15:30:02 +0200

    fix: remove Logical condition on process.execArgv and arg

--- index.js
+++ index.js
@@ -6,9 +6,7 @@ const { pathToFileURL } = require('node:url')
 
 const isFastifyAutoloadTypescriptOverride = !!process.env.FASTIFY_AUTOLOAD_TYPESCRIPT
 const isTsNode = (Symbol.for('ts-node.register.instance') in process) || !!process.env.TS_NODE_DEV
-const isBabelNode = (/* istanbul ignore next */process.execArgv || [])
-  .concat(/* istanbul ignore next */process.argv || [])
-  .some((arg) => arg.indexOf('babel-node') >= 0)
+const isBabelNode = process.execArgv.concat(process.argv).some((arg) => arg.indexOf('babel-node') >= 0)
 
 const isVitestEnvironment = process.env.VITEST === 'true' || process.env.VITEST_WORKER_ID !== undefined
 const isJestEnvironment = process.env.JEST_WORKER_ID !== undefined
