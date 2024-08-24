5489d2c8a2eba05af55575db1c50a6692576c789
Alex Miller <codex.nz@gmail.com>
2024-08-25 04:21:45 +1200

    test: use npm by default

--- scripts/unit.js
+++ scripts/unit.js
@@ -3,7 +3,7 @@
 const { exec } = require('node:child_process')
 const { argv } = require('node:process')
 
-const child = exec(argv[2] + ' run unit:with-modules')
+const child = exec((argv[2] ?? 'npm') + ' run unit:with-modules')
 
 child.stdout.pipe(process.stdout)
 child.stderr.pipe(process.stderr)
