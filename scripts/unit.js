5a6d882c521f32751acd1dbaab0859fd8d9eb779
Alex Miller <codex.nz@gmail.com>
2024-08-25 02:22:46 +1200

    feat: Add Bun and Deno to typescript support check

--- scripts/unit.js
+++ scripts/unit.js
@@ -1,8 +1,9 @@
 'use strict'
 
 const { exec } = require('node:child_process')
+const { argv } = require('node:process')
 
-const child = exec('npm run unit:with-modules')
+const child = exec(argv[2] + ' run unit:with-modules')
 
 child.stdout.pipe(process.stdout)
 child.stderr.pipe(process.stderr)
