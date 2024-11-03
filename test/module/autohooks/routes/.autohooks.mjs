f6ea78351ff1595c95a5234d8152d691a94fb76c
Frazer Smith <frazer.dev@icloud.com>
2024-11-03 10:56:24 +0000

    style: remove trailing whitespace

--- test/module/autohooks/routes/.autohooks.mjs
+++ test/module/autohooks/routes/.autohooks.mjs
@@ -1,4 +1,4 @@
-export default async function (app, opts) {    
+export default async function (app, opts) {
   app.addHook('onRequest', async (req, reply) => {
     req.hooked = req.hooked || []
     req.hooked.push('root')
