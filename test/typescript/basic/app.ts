26d60ea5a7418c3ee458b859a0e7903a0215ff48
Frazer Smith <frazer.dev@icloud.com>
2025-01-11 12:44:17 +0000

    test: prefix unused params with underscores (#437)

--- test/typescript/basic/app.ts
+++ test/typescript/basic/app.ts
@@ -2,7 +2,7 @@ import { FastifyPluginCallback } from 'fastify'
 import { join } from 'node:path'
 import fastifyAutoLoad from '../../../'
 
-const app: FastifyPluginCallback = function (fastify, opts, next): void {
+const app: FastifyPluginCallback = function (fastify, _opts, next): void {
   fastify.register(fastifyAutoLoad, {
     dir: join(__dirname, 'foo'),
   })
--- test/typescript/basic/foo/typescript.ts
+++ test/typescript/basic/foo/typescript.ts
@@ -1,7 +1,7 @@
 import { FastifyPluginCallback } from 'fastify'
 
-const plugin: FastifyPluginCallback = function (fastify, opts, next): void {
-  fastify.get('/typescript', (request, reply): void => {
+const plugin: FastifyPluginCallback = function (fastify, _opts, next): void {
+  fastify.get('/typescript', (_request, reply): void => {
     reply.send({ script: 'type' })
   })
 
