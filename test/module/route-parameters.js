12ff679e7ee55ce998b57b3a345ebeaec27e3756
Samuele Salvatico <samuele.salvatico@nearform.com>
2025-01-18 19:34:42 +0100

    test: fixed /route-parameters

--- test/module/route-parameters.js
+++ test/module/route-parameters.js
@@ -16,31 +16,41 @@ describe('routeParams: Default behaviour', function () {
     await app.close()
   })
 
-  it('should handle routes correctly', async function () {
-    let res = await app.inject('/')
+  it('should handle root route correctly', async function () {
+    const res = await app.inject('/')
     assert.strictEqual(res.statusCode, 200)
     assert.deepStrictEqual(res.json(), { route: '/' })
+  })
 
-    res = await app.inject('/pages')
+  it('should handle /pages route correctly', async function () {
+    const res = await app.inject('/pages')
     assert.strictEqual(res.statusCode, 200)
     assert.deepStrictEqual(res.json(), { route: '/pages' })
+  })
 
-    res = await app.inject('/pages/archived')
+  it('should handle /pages/archived route correctly', async function () {
+    const res = await app.inject('/pages/archived')
     assert.strictEqual(res.statusCode, 200)
     assert.deepStrictEqual(res.json(), { route: '/pages/archived' })
+  })
 
-    res = await app.inject('/pages/test_id')
+  it('should handle /pages/:id route correctly', async function () {
+    const res = await app.inject('/pages/test_id')
     assert.strictEqual(res.statusCode, 200)
     assert.deepStrictEqual(res.json(), { route: '/pages/:id/', id: 'test_id' })
+  })
 
-    res = await app.inject('/pages/test_id/edit')
+  it('should handle /pages/:id/edit route correctly', async function () {
+    const res = await app.inject('/pages/test_id/edit')
     assert.strictEqual(res.statusCode, 200)
     assert.deepStrictEqual(res.json(), {
       route: '/pages/:id/edit',
       id: 'test_id',
     })
+  })
 
-    res = await app.inject('/users/test_id/details')
+  it('should handle /users/:id/details route correctly', async function () {
+    const res = await app.inject('/users/test_id/details')
     assert.strictEqual(res.statusCode, 200)
     assert.deepStrictEqual(res.json(), {
       route: '/users/:id/details',
@@ -61,37 +71,53 @@ describe('routeParams: off', function () {
     await app.close()
   })
 
-  it('should handle routes correctly', async function () {
-    let res = await app.inject('/')
+  it('should handle root route correctly', async function () {
+    const res = await app.inject('/')
     assert.strictEqual(res.statusCode, 200)
     assert.deepStrictEqual(res.json(), { route: '/' })
+  })
 
-    res = await app.inject('/pages')
+  it('should handle /pages route correctly', async function () {
+    const res = await app.inject('/pages')
     assert.strictEqual(res.statusCode, 200)
     assert.deepStrictEqual(res.json(), { route: '/pages' })
+  })
 
-    res = await app.inject('/pages/archived')
+  it('should handle /pages/archived route correctly', async function () {
+    const res = await app.inject('/pages/archived')
     assert.strictEqual(res.statusCode, 200)
     assert.deepStrictEqual(res.json(), { route: '/pages/archived' })
+  })
 
-    res = await app.inject('/pages/test_id')
+  it('should handle /pages/test_id route correctly', async function () {
+    const res = await app.inject('/pages/test_id')
     assert.strictEqual(res.statusCode, 404)
+  })
 
-    res = await app.inject('/pages/test_id/edit')
+  it('should handle /pages/test_id/edit route correctly', async function () {
+    const res = await app.inject('/pages/test_id/edit')
     assert.strictEqual(res.statusCode, 404)
+  })
 
-    res = await app.inject('/pages/_id')
+  it('should handle /pages/_id route correctly', async function () {
+    const res = await app.inject('/pages/_id')
     assert.strictEqual(res.statusCode, 200)
     assert.deepStrictEqual(res.json(), { route: '/pages/:id/' })
+  })
 
-    res = await app.inject('/pages/_id/edit')
+  it('should handle /pages/_id/edit route correctly', async function () {
+    const res = await app.inject('/pages/_id/edit')
     assert.strictEqual(res.statusCode, 200)
     assert.deepStrictEqual(res.json(), { route: '/pages/:id/edit' })
+  })
 
-    res = await app.inject('/users/test_id/details')
+  it('should handle /users/test_id/details route correctly', async function () {
+    const res = await app.inject('/users/test_id/details')
     assert.strictEqual(res.statusCode, 404)
+  })
 
-    res = await app.inject('/users/_id/details')
+  it('should handle /users/_id/details route correctly', async function () {
+    const res = await app.inject('/users/_id/details')
     assert.strictEqual(res.statusCode, 200)
     assert.deepStrictEqual(res.json(), { route: '/users/:id/details' })
   })
