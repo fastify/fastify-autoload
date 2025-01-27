eaedfe039d0fec74d4011cd8f03bc1319dd94052
jean <jean.antoine.michelet@gmail.com>
2025-01-27 20:52:59 +0100

    revert: style changes

--- test/issues/369/test.js
+++ test/issues/369/test.js
@@ -10,7 +10,7 @@ test('Should throw an error when trying to load invalid hooks', async (t) => {
   const app = Fastify()
   app.register(autoload, {
     dir: path.join(__dirname, 'invalid-autohooks'),
-    autoHooks: true,
+    autoHooks: true
   })
 
   await t.rejects(app.ready(), /Invalid or unexpected token/)
@@ -20,34 +20,26 @@ test('Should throw an error when trying to import hooks plugin using index.ts if
   const app = Fastify()
   app.register(autoload, {
     dir: path.join(__dirname, 'invalid-index-type'),
-    autoHooks: true,
+    autoHooks: true
   })
 
   if (runtime.nodeVersion >= 23) {
     t.doesNotThrow(() => app.ready())
   } else {
-    await t.rejects(
-      app.ready(),
-      new Error(
-        `@fastify/autoload cannot import hooks plugin at '${path.join(
-          __dirname,
-          'invalid-index-type/index.ts'
-        )}'`
-      )
-    )
+    await t.rejects(app.ready(), new Error(`@fastify/autoload cannot import hooks plugin at '${path.join(__dirname, 'invalid-index-type/index.ts')}'`))
   }
 })
 
-test("Should not accumulate plugin if doesn't comply to matchFilter", async (t) => {
+test('Should not accumulate plugin if doesn\'t comply to matchFilter', async (t) => {
   const app = Fastify()
   app.register(autoload, {
-    dir: path.join(__dirname, 'routes'),
+    dir: path.join(__dirname, 'routes')
   })
 
   await app.ready()
 
   const res = await app.inject({
-    url: '/',
+    url: '/'
   })
 
   t.equal(res.statusCode, 200)
@@ -55,13 +47,13 @@ test("Should not accumulate plugin if doesn't comply to matchFilter", async (t) 
   const app2 = Fastify()
   app2.register(autoload, {
     dir: path.join(__dirname, 'routes'),
-    matchFilter: /invalid/,
+    matchFilter: /invalid/
   })
 
   await app2.ready()
 
   const res2 = await app2.inject({
-    url: '/',
+    url: '/'
   })
 
   t.equal(res2.statusCode, 404)
@@ -71,13 +63,13 @@ test('Should be able to filter paths using a string', async (t) => {
   const app = Fastify()
   app.register(autoload, {
     dir: path.join(__dirname, 'routes'),
-    matchFilter: 'routes.js',
+    matchFilter: 'routes.js'
   })
 
   await app.ready()
 
   const res = await app.inject({
-    url: '/',
+    url: '/'
   })
 
   t.equal(res.statusCode, 200)
@@ -85,13 +77,13 @@ test('Should be able to filter paths using a string', async (t) => {
   const app2 = Fastify()
   app2.register(autoload, {
     dir: path.join(__dirname, 'routes'),
-    matchFilter: 'invalid-path',
+    matchFilter: 'invalid-path'
   })
 
   await app2.ready()
 
   const res2 = await app2.inject({
-    url: '/',
+    url: '/'
   })
 
   t.equal(res2.statusCode, 404)
@@ -101,13 +93,13 @@ test('Should be able to filter paths using a function', async (t) => {
   const app = Fastify()
   app.register(autoload, {
     dir: path.join(__dirname, 'routes'),
-    matchFilter: (path) => path.includes('routes.js'),
+    matchFilter: (path) => path.includes('routes.js')
   })
 
   await app.ready()
 
   const res = await app.inject({
-    url: '/',
+    url: '/'
   })
 
   t.equal(res.statusCode, 200)
@@ -115,13 +107,13 @@ test('Should be able to filter paths using a function', async (t) => {
   const app2 = Fastify()
   app2.register(autoload, {
     dir: path.join(__dirname, 'routes'),
-    matchFilter: (path) => path.includes('invalid-path'),
+    matchFilter: (path) => path.includes('invalid-path')
   })
 
   await app2.ready()
 
   const res2 = await app2.inject({
-    url: '/',
+    url: '/'
   })
 
   t.equal(res2.statusCode, 404)
@@ -131,13 +123,13 @@ test('Should not accumulate plugin if ignoreFilter is matched', async (t) => {
   const app = Fastify()
   app.register(autoload, {
     dir: path.join(__dirname, 'routes'),
-    ignoreFilter: /\/not-exists.js/,
+    ignoreFilter: /\/not-exists.js/
   })
 
   await app.ready()
 
   const res = await app.inject({
-    url: '/',
+    url: '/'
   })
 
   t.equal(res.statusCode, 200)
@@ -146,13 +138,13 @@ test('Should not accumulate plugin if ignoreFilter is matched', async (t) => {
   app2.register(autoload, {
     dir: path.join(__dirname, 'routes'),
     ignoreFilter: /\/routes.js/,
-    autoHooks: true,
+    autoHooks: true
   })
 
   await app2.ready()
 
   const res2 = await app2.inject({
-    url: '/',
+    url: '/'
   })
 
   t.equal(res2.statusCode, 404)
@@ -163,7 +155,7 @@ test('Should not set skip-override if hook plugin is not a function or async fun
   app.register(autoload, {
     dir: path.join(__dirname, 'routes'),
     autoHooks: true,
-    cascadeHooks: true,
+    cascadeHooks: true
   })
 
   app.decorateRequest('hooked', '')
@@ -171,14 +163,14 @@ test('Should not set skip-override if hook plugin is not a function or async fun
   await app.ready()
 
   const res = await app.inject({
-    url: '/child',
+    url: '/child'
   })
 
   t.equal(res.statusCode, 200)
   t.same(JSON.parse(res.payload), { hooked: ['root', 'child'] })
 
   const res2 = await app.inject({
-    url: '/promisified',
+    url: '/promisified'
   })
 
   t.equal(res2.statusCode, 200)
@@ -189,7 +181,7 @@ test('Should not enrich non-SyntaxError', async (t) => {
   const app = Fastify()
   app.register(autoload, {
     dir: path.join(__dirname, 'non-SyntaxError'),
-    autoHooks: true,
+    autoHooks: true
   })
 
   t.rejects(app.ready(), new ReferenceError('x is not defined'))
