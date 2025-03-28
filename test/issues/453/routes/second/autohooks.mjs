69d8c54939f41f8e5980bf154fa00eba886b180b
MrWook <mr.wook@web.de>
2025-03-28 07:42:25 +0100

    chore: add comment to not delete a not used route

--- test/issues/453/routes/second/autohooks.mjs
+++ test/issues/453/routes/second/autohooks.mjs
@@ -1,3 +1,5 @@
+// This folder is not used directly by the test, but in the previous implementation, the last hook used would affect the outcome.
+// This folder ensures that the test fails if the fix is reverted or modified incorrectly.
 const pluginRegister = async (fastify) => {
   fastify.addHook('onRequest', async (request) => {
     request.hooksUsed.push('second')
