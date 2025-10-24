de464d38b2b1ce87dd3edd89c6a77cdb734ae1f4
Aras Abbasi <aras.abbasi@googlemail.com>
2025-10-25 00:46:49 +0200

    feat: add bun and deno

--- test/typescript-esm/forceESM.ts
+++ test/typescript-esm/forceESM.ts
@@ -1,4 +1,4 @@
-import test, { describe, before, after } from 'node:test'
+import { describe, test } from 'node:test'
 import assert from 'node:assert'
 import fastify from 'fastify'
 
@@ -10,18 +10,15 @@ const __dirname = dirname(fileURLToPath(import.meta.url))
 
 describe('typescript/basic test suite', function () {
   const app = fastify()
-  before(async function () {
-    app.register(fastifyAutoLoad, { dir: resolve(__dirname, 'app'), forceESM: true })
-    await app.ready()
-  })
-
-  after(async function () {
-    await app.close()
-  })
 
   test('should load routes and respond correctly', async function () {
+    app.register(fastifyAutoLoad, { dir: resolve(__dirname, 'app'), forceESM: true })
+    await app.ready()
+
     const res = await app.inject({ url: '/installed' })
     assert.strictEqual(res.statusCode, 200)
     assert.deepStrictEqual(JSON.parse(res.payload), { result: 'ok' })
+
+    await app.close()
   })
 })
